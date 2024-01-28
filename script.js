f7af3a1d801def95321c19775919989721d13185
amangithub9 <iamaman770@gmail.com>
2024-01-28 22:17:52 +0530

    BMI Project Done!

---
+++ script.js
@@ -0,0 +1,9 @@
+function bmi() {
+  let h = document.getElementById("height").value;
+  let w = document.getElementById("weight").value;
+
+  let bmi = w / (((h / 100) * h) / 100);
+  let total = bmi.toFixed(2);
+  console.log(total);
+  document.getElementById("result").innerHTML = "Your BMI is " + total;
+}
